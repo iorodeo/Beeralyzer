720312bcee9b4a9096bc8e02bbb44daf0a99bf38
Jean-Fran?ois Th?or?t <jftheoret@gmail.com>
2013-11-11 17:57:53 -0500

    Added measure notes interface

---
+++ src/beeralyzer/gui/measure_notes.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 11, 2013
+
+@author: jftheoret
+'''
+
+from PyQt4.QtGui import QDialog
+from PyQt4 import uic
+from PyQt4 import QtGui
+
+class MeasureNotes(QDialog):
+
+    def __init__(self, parent):
+        QtGui.QMainWindow.__init__(self, parent)
+        self.ui = uic.loadUi('measure_notes.ui', self)
+        self.setModal(True)
+        self.setWindowTitle("Enter Measure Notes")
